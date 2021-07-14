7bef03c9e990053407f59ca21659ebc8a752bb30
Junjie <james9145@hotmail.com>
2021-07-14 12:09:41 +0800

    finished randomizer and delete function for students

--- app.js
+++ app.js
@@ -5,21 +5,29 @@ const randomStudent = document.getElementById("student");
 const randomizer = document.querySelector(".randomizer");
 const nameList = document.querySelector(".studentlist");
 let studentList = [];
+let randomList = [];
 
 //eventlisteners
-addStudent.addEventListener("click", pushStudent)
+addStudent.addEventListener("click", pushStudent);
 randomizer.addEventListener("click", randomChoice);
+
 
 //functions
 function pushStudent(e) {
     e.preventDefault();
     if (studentName.value != "") {
         studentList.push(studentName.value);
-        const x = document.createElement("li");;
-        const text = document.createTextNode(studentName.value.toString());
-        x.appendChild(text);
+        //make the list
+        const x = document.createElement("li");
+        x.innerText = studentName.value;
         x.classList.add("student-list-child");
-        nameList.appendChild(x);   
+        nameList.appendChild(x);
+        //create delete button
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("deletebtn");
+        deleteBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+        x.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click", deleteStudent);
         studentName.value ="";
     } else {
         alert ("Please enter a student's name")
@@ -28,7 +36,24 @@ function pushStudent(e) {
 
 function randomChoice (e) {
     e.preventDefault();
-    let choice = Math.floor(Math.random() * studentList.length);
-    randomStudent.innerHTML = studentList[choice].toString();
+    if (randomList.length == 0) {
+        studentList.forEach((item) => {
+            randomList.push(item);
+        });
+    }
+    console.log(randomList, "before");
+    let choice = Math.floor(Math.random() * randomList.length);
+    randomStudent.innerHTML = randomList[choice].toString();
+    randomList.splice(choice, 1)
+    console.log(randomList, "after")
 }
 
+function deleteStudent (e) {
+    console.log(studentList)
+    const item = e.target;
+    const student = item.parentNode;
+    const studentIndex = studentList.indexOf(student.innerText);
+    studentList.splice(studentIndex, 1);
+    student.remove();
+    console.log(studentList)
+}
